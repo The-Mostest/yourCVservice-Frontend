352acf8095488eaffb6ed92da7dcafbe00486527
The-Mostest <fbmcdougall@gmail.com>
2024-11-15 13:43:30 +0000

    Added Component Library and a burger button

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,40 @@
 
-
+import { useDisclosure } from '@mantine/hooks'
+import { Burger } from '@mantine/core'
 
 
 const NavBar = () => {
 
-
+    const [opened, { toggle }] = useDisclosure()
+    console.log(opened)
 
 
     return (
         <>
+
+
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" style={{position: 'fixed'}} />
+
+            {opened && (
+                <>
+
+                    <img src="/images/logo.png" alt="Website Logo" style={{ width: "100px", height: "auto" }} />
+                    <h1>Link</h1>
+                    <h1>Link</h1>
+                    <h1>Link</h1>
+                    <h1>Link</h1>
+
+                </>
+
+            )}
+
+
+
+
+
+
+
         </>
     )
 }
