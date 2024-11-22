70944021d22819e727754286749933f39184875d
The-Mostest <fbmcdougall@gmail.com>
2024-11-22 13:13:29 +0000

    SCSS

--- src/Components/calender.jsx
+++ src/Components/calender.jsx
@@ -1,21 +1,58 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import { useEffect, useState } from 'react'
+
+import { indexInterview } from '../services/interviewServices'
+
+import styles from './calendar.module.scss'
 
 
-export default function Calendar({user}) {
+
+
+
+
+
+export default function Calendar() {
+
+    const [interviews, setInterviews] = useState([])
+
+
+    useEffect(() => {
+        const fetchInterviews = async () => {
+            try {
+                const { data } = await indexInterview()
+                setInterviews(data)
+
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        fetchInterviews()
+    }, [])
+
+    const events = interviews.map((interview) => ({
+        title: `Interview with ${interview.company}`,
+        start: interview.date,
+        end: interview.end_date || null,
+
+    }));
+
+
+
+
+
+
     return (
+
+
+        <div className={styles.calendarContainer}>
+            
         <FullCalendar
+            defaultView="dayGridMonth"
             plugins={[dayGridPlugin]}
-            eventContent={renderEventContent}
-        />
-    )
-}
-
-function renderEventContent(eventInfo) {
-    return (
-        <>
-            <b>{eventInfo.timeText = user.interview}</b>
-            <i>{eventInfo.event.title}</i>
-        </>
+            events={events}
+            />
+            </div>
     )
 }
